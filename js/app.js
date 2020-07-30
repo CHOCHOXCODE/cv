9e2bb7c715d5a1ca3cdf0b8e3810641cff691857
CHOCHOXCODE <xubayer.chocho@gmail.com>
2020-07-31 03:24:27 +0600

    update counter

--- js/app.js
+++ js/app.js
@@ -11,7 +11,56 @@ $(document).ready(function(){
         let tooltip = bar.children[0];
         tooltip.innerText = percentage + '%';
         bar.style.width = percentage + '%'
-        console.log(percentage);
+        
     });
+
+    // counter
+    const counters = document.querySelectorAll('.counter');
+    console.log(counters)
+    function runCounter () {
+        counters.forEach(counter =>{
+            counter.innerText = 0;
+            let target = +counter.dataset.count;
+            let step = target / 100;
+
+            
+            
+
+            let countIt = function () {
+                let displayedCount = +counter.innerText;
+                if(displayedCount < target){
+                    counter.innerText = Math.ceil(displayedCount + step);
+                    
+                    setTimeout(countIt, 1);
+                } 
+                else {
+                    counter.innerText = target;
+                }
+            }
+            countIt();
+
+        })
+    }
+
+    
+
+    let counterSection = document.querySelector('.counter_wrapper');
+
+    let options = {
+        rootMargin : '0px 0px -300px 0px'
+    }
+
+    let done = 0;
+
+    const sectionObserver = new IntersectionObserver(function(entries) {
+
+        if(entries[0].isIntersecting && done !==1) {
+            done = 1;
+            runCounter();
+        }
+    }, options)
+
+
+    sectionObserver.observe(counterSection)
 
 });
